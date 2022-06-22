07749277ac3fdd8f36cae2e330dce319eb682dca
Tetiana Bahyra <bagiratana@gmail.com>
2022-06-22 17:06:32 +0300

    Deleted folder test,add gem rspec and modified aplication.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module InnovationForpost
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
