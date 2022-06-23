eb0bb229d6400b56e7e46e8518040b961976e375
stasfly <s19f84@gmail.com>
2022-06-23 12:57:26 +0300

    has_secure_password added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
                     uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }#, allow_nil: true
 
-  
+  has_secure_password
   
   
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_110742) do
+ActiveRecord::Schema.define(version: 2022_06_23_075704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_110742) do
     t.text "role", default: "user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "password_digest"
   end
 
 end
