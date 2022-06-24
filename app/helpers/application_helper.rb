5524c9fdab03baec125ce37d83e328b90e387c01
stasfly <s19f84@gmail.com>
2022-06-24 10:59:42 +0300

    full title is added to views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+# Returns the full title on a per-page basis.
+def full_title(page_title = '')
+  base_title = "INNOVATION FORPOST"
+  if page_title.empty?
+    base_title
+  else
+    page_title + " | " + base_title
+  end
+end
+
 end
