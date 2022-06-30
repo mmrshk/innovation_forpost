10d1a3592c557d6f6caa8bba0681aba9229d21d8
Emma <emma.yeroshek@gmail.com>
2022-06-30 16:06:19 +0300

    Add lit gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Lit::Engine => '/lit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # devise said he wants to have a specified root rout, so:
