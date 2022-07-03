307394ccc882127fe29b1d963cd5c2d60e5b86d8
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-07-04 00:50:26 +0300

    Fix unprotected redirect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to @article(:only_path => true)
     else
       render :edit, status: :unprocessable_entity
     end
