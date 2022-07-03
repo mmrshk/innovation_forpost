37b6d559879664452c8108271fc629b37b6705de
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-07-03 17:49:28 +0300

    Cleaning up comments in migrates

--- db/migrate/20220629215533_add_status_to_articles.rb
+++ db/migrate/20220629215533_add_status_to_articles.rb
@@ -1,7 +1,5 @@
 class AddStatusToArticles < ActiveRecord::Migration[6.0]
   def change
-    # create_enum "statuses", %w[draft published trashed]
-    # add_column :articles, :status, :statuses
     add_column :articles, :status, :integer, default: 0
   end
 end
--- db/migrate/20220630141054_add_language_to_articles.rb
+++ db/migrate/20220630141054_add_language_to_articles.rb
@@ -1,7 +1,5 @@
 class AddLanguageToArticles < ActiveRecord::Migration[6.0]
   def change
-    # create_enum "languages", %w[ukr eng]
-    # add_column :articles, :language, :languages
     add_column :articles, :language, :integer, default: 0
   end
 end
