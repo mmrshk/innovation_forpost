b3ccb4347d231ede83297338d198d6339f1d041f
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-07-04 00:17:10 +0300

    Add new articles_controller.rb

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id]).destroy
+    flash[:success] = "The article was successfully destroyed."
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text, :user_id, :language, :status)
+    end
+end
