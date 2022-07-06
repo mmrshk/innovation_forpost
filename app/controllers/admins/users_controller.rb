79e06d94a3d90220c6f36a3d6afd0c2cd3b8b7bc
stasfly <s19f84@gmail.com>
2022-07-06 17:25:09 +0300

    Finish _RUD operations for user. C___ does not work yet.

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,13 +1,14 @@
 module Admins
   class UsersController < AdminsController
-    before_action :current_user, :logged_in_user
+    before_action :current_user, :super_admin?
     before_action :authenticate_user!
     before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :super_admin?, only: [:index, :edit, :update, :destroy]
 
 
 
     def index
-      @users = User.all
+      @users = User.all if super_admin?
     end
 
     def show
@@ -15,25 +16,33 @@ module Admins
 
     def new
       @user = User.new
+      @path = '/admins/users/new'
     end
-
+    
     def create
       @user = User.new(user_params)
       @user.role = 'user'
+      if @user.save
+        redirect_to admins_user(@user.id)
+      else
+        render :edit, status: :unprocessable_entity
+        redirect_to root_path
+      end
     end 
 
     def edit
+      @path = "/admins/users/#{@user.id}"
     end
 
     def update
-      unless User.find_by(id: params[:id]).role == 'super_admin' && last_super_admin? && params[:user][:role] != 'super_admin'
+      if User.find_by(id: params[:id]).role == 'super_admin' && last_super_admin? && params[:user][:role] == 'super_admin'
+        redirect_to admins_user_url(@user), notice: "You cannot change a super_admin status being the last one."
+      else
         if @user.update(user_params)
           redirect_to admins_user_url(@user), notice: "User was successfully updated."
         else
           render :edit, status: :unprocessable_entity
         end
-      else
-        redirect_to admins_user_url(@user), notice: "You cannot change a super_admin status being the last one."
       end
     end
 
@@ -43,14 +52,7 @@ module Admins
     end
 
 
-
-    
-
     private
-
-    def set_user
-      @user = User.find_by(id: params[:id])
-    end
 
     def user_params
       params.require(:user).permit(:name, :email, :phone_number, :role, :password, :password_confirmation)
--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,79 +1,32 @@
 class AdminsController < ApplicationController
-  before_action :current_user, :logged_in_user
+  before_action :current_user, :super_admin?, :admin?
   before_action :authenticate_user!
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :check_if_super_admmin?, only: [:users_show, :edit, :update, :destroy]
+  
+  helper_method :super_admin?, :admin?, :last_super_admin?
   
   
-  def users_show
-    @users = User.all
-  end
 
-  def articles_show
-    if @logged_in_user.role == 'super_admin' || 'admin'
-      #@articles ||= Articles.all
-    end
-  end
-
-  def show
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def edit
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.role = 'user'
-  end 
-
-  def update
-    unless User.find_by(id: params[:id]).role == 'super_admin' && last_super_admin? && params[:user][:role] != 'super_admin'
-      if @user.update(user_params)
-        redirect_to user_url(@user), notice: "User was successfully updated."
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    else
-      redirect_to user_url(@user), notice: "You cannot change a super_admin status being the last one."
-    end
-  end
-
-  def destroy
-    @user.destroy unless last_super_admin?
-    redirect_to root_path    
-  end
-
-  def logged_in_user
-    @logged_in_user = current_user
-  end
-
-  def check_if_super_admmin?
+  private
+    
+  def super_admin?
     current_user.role == 'super_admin'
   end
   
-  private
-    def set_user
-      @user = User.find_by(id: params[:id])
-    end
+  def admin?
+    current_user.role == 'admin'
+  end
 
-    
-
-    
-
-    def last_super_admin?
-      super_admin_count = 0
-      @users ||= User.all
-      @users.map { |user|
-        super_admin_count += 1 if user.role == 'super_admin'
-      }
-      true if super_admin_count < 2
-    end
-
-    def user_params
-      params.require(:user).permit(:name, :email, :phone_number, :role, :password, :password_confirmation)
-    end
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def last_super_admin?
+    super_admin_count = 0
+    @users ||= User.all
+    @users.map { |user|
+      super_admin_count += 1 if user.role == 'super_admin'
+    }
+    true if super_admin_count < 2
+  end
+  
 end
