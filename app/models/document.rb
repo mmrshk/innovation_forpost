1e8cd963d0143f180f11d9eabae94681a5d10d37
Tetiana Bahyra <bagiratana@gmail.com>
2022-07-12 13:07:56 +0300

    Test rubocop

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Document < ApplicationRecord
   validates :name, presence: true
 
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  it "is valid with a name" do
+  it 'is valid with a name' do
     document = Document.new(
-    name: "Test document",
+      name: 'Test document'
     )
     expect(document).to be_valid
   end
-
 end
