4f55d9cbfe923928a53733dc0aa2fff68ceec1c6
stasfly <s19f84@gmail.com>
2022-07-13 10:20:44 +0300

    last commit on this branch

--- app/controllers/admins/articles_conrloller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Admins
-  class UsersController < AdminsController
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # enum role: {user: 0, admin: 1, super_admin: 2} _suffix: true inprogress...
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable,  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
