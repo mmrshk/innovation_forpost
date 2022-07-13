593ce95aeb1d40780064c89f7fd034bc3a59d90a
stasfly <s19f84@gmail.com>
2022-07-13 17:12:33 +0300

    Pos fix correction for admin account

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,11 +2,11 @@
 
 module Admins
   class UsersController < AdminsController
-    before_action :user, only: %i[edit show update]
+    before_action :user, only: %i[edit show update destroy]
     helper_method :last_super_admin?
 
     def index
-      @users = User.all
+      @users = User.all if current_user.role_super_admin?
     end
 
     def show; end
@@ -17,22 +17,21 @@ module Admins
 
     def create
       @user = User.new(user_params)
-
+      @user.role_user! unless current_user.role_super_admin?
       if @user.save
-        redirect_to admins_user_url(@user), notice: ''
+        redirect_to admins_user_url(@user), notice: 'New user was successfully created.'
       else
         render :new, status: :unprocessable_entity
       end
     end
 
-    def edit
-    end
+    def edit; end
 
     def update
-      redirect_to admins_user_url(@user), notice: '' if last_super_admin_tries_to_update_its_role?
-
-      if @user.update(user_params)
-        redirect_to admins_user_url(@user), notice: '' 
+      if last_super_admin_tries_to_update_its_role?
+        redirect_to admins_user_url(@user), notice: 'Prohibited! Last super_admin protestion!'
+      elsif @user.update(user_params)
+        redirect_to admins_user_url(@user), notice: 'Account was successfully apdated.'
       else
         render :edit, status: :unprocessable_entity
       end
@@ -55,10 +54,7 @@ module Admins
     end
 
     def last_super_admin?
-      @last_super_admin ||= begin
-        super_admins = User.where(role: User::USER_ROLES[:super_admin]).size 
-        super_admins < 2
-      end
+      User.where(role: User::USER_ROLES[:super_admin]).size < 2
     end
 
     def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@
 
 class User < ApplicationRecord
   USER_ROLES = {
-    user: 'user', 
-    admin: 'admin', 
+    user: 'user',
+    admin: 'admin',
     super_admin: 'super_admin'
   }.freeze
 
-  enum role: { super_admin: 0, admin: 1, user: 2 }, _prefix: :role
+  enum role: { super_admin: 2, admin: 1, user: 0 }, _prefix: :role
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
