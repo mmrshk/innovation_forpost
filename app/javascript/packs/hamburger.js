faae2b9f37cd05b38037e1b5d0d8545a7e6de2d0
Pakholchuk5431 <alanson250@gmail.com>
2022-07-15 11:18:23 +0300

    js and css fix

--- app/javascript/packs/hamburger.js
+++ app/javascript/packs/hamburger.js
@@ -1,12 +1,13 @@
-const navtrigger = document.querySelector(".nav-trigger"),
-mobilmenu = document.querySelector(".mobile-menu"),
-container = document.querySelector(".header-container"),
-top = document.querySelector(".header-top");
+const NAV_TRIGER = document.querySelector(".nav-trigger"),
+MOBILE_MENU = document.querySelector(".mobile-menu"),
+HEADER_CONTAINER = document.querySelector(".header-container"),
+HEADER_TOP = document.querySelector(".header-top");
 
-navtrigger.addEventListener("click", () => {
-  navtrigger.classList.toggle("active");
-  mobilmenu.classList.toggle("active");
-  container.classList.toggle("active");
-  container.style.boxShadow = "none";
-  top.style.padding = "30px 0";
+NAV_TRIGER.addEventListener("click", () => {
+  NAV_TRIGER.classList.toggle("active");
+  MOBILE_MENU.classList.toggle("active");
+  HEADER_CONTAINER.classList.toggle("active");
+  HEADER_CONTAINER.classList.remove("scroll");
+  HEADER_TOP.classList.toggle("active");
+  HEADER_TOP.classList.remove("scroll");
 });
--- app/javascript/packs/scroll.js
+++ app/javascript/packs/scroll.js
@@ -1,13 +1,13 @@
 window.addEventListener('scroll', () => {
-  const top = document.querySelector(".header-top"),
-  container = document.querySelector(".header-container"),
-  active = document.querySelector(".active");
+  const HEADER_TOP = document.querySelector(".header-top"),
+  HEADER_CONTAINER = document.querySelector(".header-container");
+  let active = document.querySelector(".active");
 
   if (window.scrollY > 30 && !active) {
-    top.style.padding = "10px 0";
-    container.style.boxShadow = "0 0 60px 0 #989898";
+    HEADER_TOP.classList.add("scroll");
+    HEADER_CONTAINER.classList.add("scroll");
   } else {
-    top.style.padding = "30px 0";
-    container.style.boxShadow = "none";
+    HEADER_TOP.classList.remove("scroll");
+    HEADER_CONTAINER.classList.remove("scroll");
   }
 });
