e19cf1926dbb41f1c9185b132195e5bdd21df93b
Tetiana Bahyra <bagiratana@gmail.com>
2022-07-18 17:31:14 +0300

    Resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -15,27 +15,6 @@ ActiveRecord::Schema.define(version: 2022_07_15_083937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
   create_table "article_tags", force: :cascade do |t|
     t.bigint "tag_id", null: false
     t.bigint "article_id", null: false
@@ -56,12 +35,6 @@ ActiveRecord::Schema.define(version: 2022_07_15_083937) do
     t.integer "language", default: 0
     t.index ["title"], name: "index_articles_on_title"
     t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
-  create_table "attachments", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "documents", force: :cascade do |t|
@@ -161,7 +134,6 @@ ActiveRecord::Schema.define(version: 2022_07_15_083937) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "article_tags", "articles"
   add_foreign_key "article_tags", "tags"
   add_foreign_key "articles", "users"
