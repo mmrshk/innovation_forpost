1c4c6f5f3c0977f7484cb8aea5ae4754b4806860
Andriy1328 <ankle1328@gmail.com>
2022-07-19 10:48:50 +0300

    fix errors spec

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Errors", type: :request do
-  describe "GET /not_found" do
-    it "returns http success" do
-      get "/errors/not_found"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Errors', type: :request do
+  describe 'GET /not_found' do
+    it 'returns http not_found' do
+      get '/404'
+      expect(response).to have_http_status(:not_found)
     end
   end
 
-  describe "GET /unprocessable_entity" do
-    it "returns http success" do
-      get "/errors/unprocessable_entity"
-      expect(response).to have_http_status(:success)
+  describe 'GET /unprocessable_entity' do
+    it 'returns http unprocessable_entity' do
+      get '/422'
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 
-  describe "GET /internal_server_error" do
-    it "returns http success" do
-      get "/errors/internal_server_error"
-      expect(response).to have_http_status(:success)
+  describe 'GET /internal_server_error' do
+    it 'returns http internal_server_error' do
+      get '/500'
+      expect(response).to have_http_status(:internal_server_error)
     end
   end
-
 end
