5d1c5ae8b6217ab8eae95379c49890ba147ac02e
Viktoriia Horobets <marakya750@gmail.com>
2022-07-24 23:14:20 +0300

    fixed spec update

--- spec/features/update_attachment_spec.rb
+++ spec/features/update_attachment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Updating an attachment', type: :feature do
     attachment = create(:attachment)
     visit admins_attachments_path
     click_on 'Edit'
-    fill_in 'Name', with: 'NewName'
+    fill_in I18n.t('admin.attachments.name'), with: 'NewName'
     click_on 'Save'
     expect(page).to have_content('NewName')
   end
