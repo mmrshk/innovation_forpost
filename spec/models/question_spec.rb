018ec82113f3caf3d579eca20b012e50dcd170df
bogdanrymar <bogdanrymar97@gmail.com>
2022-07-25 16:51:22 +0300

    change test description and add some spaces

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,8 +11,10 @@ RSpec.describe Question, type: :model do
     it { is_expected.to validate_presence_of(:user_name) }
     it { is_expected.to validate_length_of(:user_name).is_at_least(2).is_at_most(255) }
     it { is_expected.to validate_length_of(:user_email).is_at_most(255) }
+
     let(:question_invalid_email) { Question.new(user_name: 'abc', user_email: '123.com', body: 'abc', title: 'abc') }
-    it 'can be saved' do
+
+    it ' question can not be saved if email invalid' do
       expect(question_invalid_email.save).to be_falsey
     end
   end
