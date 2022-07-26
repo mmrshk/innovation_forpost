1d1e54a7209d30b9813b88207144bfda2234a90e
bogdanrymar <bogdanrymar97@gmail.com>
2022-07-26 15:38:28 +0300

    remove require factorybot is spec and edit spaces

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'factory_bot_rails'
+
 RSpec.describe Question, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
@@ -12,7 +12,7 @@ RSpec.describe Question, type: :model do
     it { is_expected.to validate_length_of(:user_name).is_at_least(2).is_at_most(255) }
     it { is_expected.to validate_length_of(:user_email).is_at_most(255) }
 
-    let(:question_with_invalid_email) { FactoryBot.build(:question, :question_with_invalid_email) }
+    let(:question_with_invalid_email) { build(:question, :question_with_invalid_email) }
     it 'question can not be saved if email is invalid' do
       expect(question_with_invalid_email.save).to be_falsey
     end
