b8d5869739c0681657dfbbf18525b34a6e23017d
bogdanrymar <bogdanrymar97@gmail.com>
2022-07-26 10:24:43 +0300

    add CRUD for Questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,43 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      # flash.now[:alert] = 'Error Please check your input'
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find_by(id: params[:id])
+  end
+
+  def edit
+    @question = Question.find_by(id: params[:id])
+  end
+
+  def update
+    @question = Question.find_by(id: params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find_by(id: params[:id])
+    @question.destroy
+    redirect_to questions_path, notice: 'Question destroyed'
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body, :user_name, :user_email)
+  end
 end
