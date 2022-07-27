20d8b4b6b359a90a7f57205f5501700784e5a3d2
bogdanrymar <bogdanrymar97@gmail.com>
2022-07-27 16:14:06 +0300

    Revert "add changes"
    This reverts commit e9e8c2e468ce6b5fa052cf3a87bb6248de79f3b6.

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,5 +6,7 @@ class Question < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2, maximum: 255 }
   validates :body, presence: true, length: { minimum: 2, maximum: 1000 }
   validates :user_name, presence: true, length: { minimum: 2, maximum: 255 }
-  validates :user_email, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, if: proc { |question| question.user_email.present? }
+  validates :user_email, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, if: proc { |question|
+                                                                                                      question.user_email.present?
+                                                                                                    }
 end
