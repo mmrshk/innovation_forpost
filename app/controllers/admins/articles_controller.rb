855f2bb97765a23a1b476a5e67d41ec273ef9b19
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-07-27 14:47:24 +0300

    New megre views. Part 1

--- app/controllers/admins/articles_controller.rb
+++ app/controllers/admins/articles_controller.rb
@@ -31,8 +31,7 @@ module Admins
     def update
       @form = Articles::CreateUpdateForm.new(params: article_params, article: article)
       if @form.save
-        locale = @form.article.language.to_sym
-        redirect_to article_path(@form.article, locale: locale), notice: I18n.t('admins.articles.update_success')
+        redirect_to admins_articles_path, notice: I18n.t('admins.articles.update_success')
       else
         flash.now[:alert] = I18n.t('admins.articles.update_unsuccess')
         render :edit, status: :unprocessable_entity
