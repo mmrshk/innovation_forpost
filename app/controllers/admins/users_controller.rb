bb17c49fd5ea31836ba89db061b1060a8a98936a
Viktoriia Horobets <marakya750@gmail.com>
2022-07-27 15:25:27 +0300

    Added messages

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -18,7 +18,8 @@ module Admins
     def create
       @user = User.new(user_params)
       if @user.save
-        redirect_to admins_user_url(@user), notice: t('admins.users.create_success')
+        flash[:success] = 'You added a new user!'
+        redirect_to admins_user_url(@user)
       else
         render :new, status: :unprocessable_entity
       end
@@ -38,7 +39,7 @@ module Admins
 
     def destroy
       @user.destroy
-
+      flash[:success] = 'You deleted user!'
       redirect_to admins_users_path
     end
 
