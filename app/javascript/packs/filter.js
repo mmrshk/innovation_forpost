912d026a19c29e9ec5e9a0023443218f96b752e9
Andriy1328 <ankle1328@gmail.com>
2022-07-27 16:19:39 +0300

    Add filter for admin page

--- app/javascript/packs/filter.js
+++ app/javascript/packs/filter.js
@@ -7,14 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
   let instances = M.Dropdown.init(elems, options);
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   let elems = document.querySelectorAll('select');
-//   // elems.addEventListener('click', (e) => {
-//   //   e.preventDefault();
-//   // })
-//   let instances = M.FormSelect.init(elems, options);
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.datepicker');
   var instances = M.Datepicker.init(elems, options);
