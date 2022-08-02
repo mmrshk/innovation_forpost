981a0c622f2fce469d6f3e1fb33f102bc234163a
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-08-02 23:54:05 +0300

    Start fix truncate

--- app/presenters/article_presenter.rb
+++ app/presenters/article_presenter.rb
@@ -1,11 +1,44 @@
 # frozen_string_literal: true
 
 class ArticlePresenter
+  include ActionView::Helpers
+  include Rails.application.routes.url_helpers
+  include ActionView::Context
+
   def initialize(article)
     @article = article
   end
 
   def all_tags
     @article.tags.present? ? @article.tags.pluck(:name).join(', ') : ''
+  end
+
+  def truncate_article_text
+    truncate(@article.text.to_s, escape: false,
+                                 length: truncate_params[:length_truncate],
+                                 separator: truncate_params[:separator_truncate]) do
+      link_to 'Continue', controller: 'articles', action: 'show', id: @article.id
+    end
+  end
+
+  private
+
+  def truncate_params
+    length_truncate = 500
+    separator_truncate = ''
+    article_text = @article.text.to_s
+    truncate_text = truncate(article_text, length: length_truncate, escape: false)
+    if truncate_text.match(/<figure[^>]+>/)
+      # separator_truncate = '</figure>'
+      length_truncate_figure = article_text.match(%r{[\s\S]*?</figure>}).to_s.length + 3
+      truncate_text_figure = truncate(article_text, length: length_truncate_figure, escape: false)
+      if truncate_text_figure.length > truncate_text.length
+        length_truncate = length_truncate_figure
+      else
+        separator_truncate = ''
+      end
+    end
+    { length_truncate: length_truncate,
+      separator_truncate: separator_truncate }
   end
 end
