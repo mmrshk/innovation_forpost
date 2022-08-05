e587e419c97326f2a97041a5a4c580647175014d
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-08-05 10:07:59 +0300

    Try 1 fix heroku

--- app/uploaders/ck_editor_image_uploader.rb
+++ app/uploaders/ck_editor_image_uploader.rb
@@ -6,13 +6,13 @@ class CkEditorImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :aws
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :aws
+  # else
+  #   storage :file
+  # end
   # storage :fog
-
+  storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
