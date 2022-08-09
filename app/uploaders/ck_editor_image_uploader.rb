d4345bb332a584456516dd35de3076335ccf7bff
stasfly <s19f84@gmail.com>
2022-08-09 10:51:59 +0300

    Fix/cred (#70)
    * Add creds to encrypted credentials

--- app/uploaders/ck_editor_image_uploader.rb
+++ app/uploaders/ck_editor_image_uploader.rb
@@ -7,12 +7,10 @@ class CkEditorImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-  storage :fog
+    storage :fog
   else
-  storage :file
+    storage :file
   end
-
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carriervawe.rb
+++ config/initializers/carriervawe.rb
@@ -1,12 +1,13 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                            # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],            # required
+    aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],            # required
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],            # required
     region:                'us-east-1'                       # to match the carrierwave and bucket region
   }
-  config.fog_directory = ENV["AWS_BUCKET"]                   # required
+  config.fog_directory = Rails.application.credentials.aws[:bucket]                  # required
   config.fog_public    = false
   config.cache_dir     = "#{Rails.root}/tmp/uploads"         # To let CarrierWave work on Heroku
   config.storage       = :fog
 end
+
