7b87a36bc30d7b2ecfbbc3252d2d6aa9cad4a67f
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-08-10 11:05:53 +0300

    Upload into fog

--- app/uploaders/ck_editor_image_uploader.rb
+++ app/uploaders/ck_editor_image_uploader.rb
@@ -11,8 +11,8 @@ class CkEditorImageUploader < CarrierWave::Uploader::Base
   # else
   #   storage :file
   # end
-  # storage :fog
-  storage :file
+  storage :fog
+  # storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
