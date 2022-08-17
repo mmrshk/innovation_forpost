00f29d856199f56787a0b418f3c314df47a460c1
stasfly <s19f84@gmail.com>
2022-08-17 11:38:58 +0300

    Fix maile preview

--- spec/mailers/previews/test_notifier_preview.rb
+++ spec/mailers/previews/test_notifier_preview.rb
@@ -3,6 +3,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/test_notifier
 class TestNotifierPreview < ActionMailer::Preview
   def test_notifier
-    TestNotifierMailer.test_sender(User.first)
+    TestNotifierMailer.with(user: User.first).test_sender
   end
 end
