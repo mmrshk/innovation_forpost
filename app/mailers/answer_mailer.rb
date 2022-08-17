946b2297a283dae1b0a4fdfb2bb894709239fd38
bogdanrymar <bogdanrymar97@gmail.com>
2022-08-17 16:56:51 +0300

    remove some default comments

--- app/mailers/answer_mailer.rb
+++ app/mailers/answer_mailer.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class AnswerMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.answer_mailer.question_answered.subject
-  #
   def question_answered
     @question = params[:question]
     @answer = params[:answer]
@@ -13,7 +8,7 @@ class AnswerMailer < ApplicationMailer
     @greeting = 'Hi'
 
     mail(
-      from: 'bogdanrymar03@outlook.com',
+      from: ENV.fetch('OUTLOOK_USERNAME'),
       to: @question.user_email
     )
   end
