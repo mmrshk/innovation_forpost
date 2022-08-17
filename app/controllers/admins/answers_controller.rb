55a18ab89fc336b928bedece2a69f2f39e53a6f1
bogdanrymar <bogdanrymar97@gmail.com>
2022-08-17 08:45:09 +0300

    start creating AnswersController

---
+++ app/controllers/admins/answers_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Admins
+  class AnswersController < AdminsController
+    before_action :set_question
+    def create
+      @answer = @question.answers.build(answer_params)
+
+      if @answer.save
+        redirect_to admins_question_path(@question), notice: 'Answer created!'
+      else
+        @answers = @question.answers.all
+        render 'admins/questions/show'
+      end
+    end
+
+    def destroy
+      answer = @question.answers.find(params[:id])
+      if answer.destroy
+        redirect_to admins_question_path, notice: 'Answer destroyed!'
+      else
+        flash[:notice] = 'Error, something goes wrong'
+      end
+    end
+
+    private
+
+    def answer_params
+      params.require(:answer).permit(:body)
+    end
+
+    def set_question
+      @question = Question.find(params[:question_id])
+    end
+  end
+end
