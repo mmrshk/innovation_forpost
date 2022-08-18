b8fc992df548c0af3ce5ab76655ef7c2babcc5aa
stasfly <s19f84@gmail.com>
2022-08-18 17:47:43 +0300

    Set correct ref to creds

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,12 +6,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-   :address              => "smtp-mail.outlook.com",
-   :port                 => 587,
-   :enable_starttls_auto => true,
-   :user_name            => ENV["OUTLOOK_USERNAME"],
-   :password             => ENV["OUTLOOK_PASSWORD"],
-   :authentication       => :login }
+    address:              'smtp.gmail.com',
+    port:                 587,
+    enable_starttls_auto: true,
+    user_name:            Rails.application.credentials.google_account[:SMTP_user_name],
+    password:             Rails.application.credentials.google_account[:SMTP_password],
+    authentication:       :plain
+  }
 
   config.after_initialize do
     Bullet.enable        = true
