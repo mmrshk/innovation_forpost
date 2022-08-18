f72de10a84ebf2603df7960135739ab0a1262f8e
bogdanrymar <bogdanrymar97@gmail.com>
2022-08-18 18:56:42 +0300

    remove extra line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # Domain for mailer urls
-  config.action_mailer.default_url_options = { host: 'innovation-forpost.herokuapp.com' }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
