31f6723b383cfc744822965877219ed1e7da1698
bogdanrymar <bogdanrymar97@gmail.com>
2022-08-18 18:57:11 +0300

    remove extra line2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
