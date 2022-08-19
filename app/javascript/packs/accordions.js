e35675c2ca22716f9728799d9da0fc9c97e61872
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-08-19 11:09:20 +0300

    Correct style

--- app/javascript/packs/accordions.js
+++ app/javascript/packs/accordions.js
@@ -1,9 +1,9 @@
 let accordions = document.querySelectorAll('.js-table-accordions');
 accordions.forEach(accordion => {
   accordion.addEventListener('click', (event) => {
-    table = accordion.parentElement.querySelectorAll('.js-table-accordions.active');
-    if(table.length > 0 ) {
-      table.forEach(item => item!= accordion && item.classList.toggle('active'));
+    tables = accordion.parentElement.querySelectorAll('.js-table-accordions.active');
+    if(tables.length > 0 ) {
+      tables.forEach(item => item!= accordion && item.classList.toggle('active'));
     };
     accordion.classList.toggle('active');
     });
