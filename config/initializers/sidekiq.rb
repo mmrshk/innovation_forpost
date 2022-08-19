90649a5f8c76b5ddb87fbb17663361ab26f97cf6
stasfly <s19f84@gmail.com>
2022-08-19 18:34:33 +0300

    Add additional config to sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,15 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], size: 5, network_timeout: 5 }
   config.redis = { password: Rails.application.credentials.sidekiq[:redis_password] }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], size: 5, network_timeout: 5 }
   config.redis = { password: Rails.application.credentials.sidekiq[:redis_password] }
+end
+
+schedule_file = "config/schedule.yml"
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
