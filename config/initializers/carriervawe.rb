b8141f80927bf1a4fa4ba6ad8bc724758c0c51b0
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-08-21 19:55:57 +0300

    Public upload images to S3

--- config/initializers/carriervawe.rb
+++ config/initializers/carriervawe.rb
@@ -12,8 +12,7 @@ else
       region:                Rails.application.credentials.aws[:region]
     }
     config.fog_directory = Rails.application.credentials.aws[:bucket]
-    # config.fog_public    = true
-    config.fog_public    = false
+    config.fog_public    = true
     config.cache_dir     = "#{Rails.root}/tmp/uploads"
     config.storage       = :fog
   end
