f45526d22c3e43adc1690b481650fba77fe3f14e
stasfly <s19f84@gmail.com>
2022-08-22 10:42:59 +0300

    Change sidekiq.rb URL setting

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.credentials.sidekiq[:redis_url], size: 5, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
   config.redis = { password: Rails.application.credentials.sidekiq[:redis_password] }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.credentials.sidekiq[:redis_url], size: 5, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
   config.redis = { password: Rails.application.credentials.sidekiq[:redis_password] }
 end
 
