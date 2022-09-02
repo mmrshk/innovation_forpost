cc3c35e07591a696a8fea92cb4e9bc461c5afe05
Andrew <56994564+Andriy1328@users.noreply.github.com>
2022-09-02 16:04:43 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
     resources :questions, only: %i[index new create show]
     get '/companies', to: 'companies#index'
   end
-  
+
   get '/404', to: 'errors#not_found'
   get '/422', to: 'errors#unprocessable_entity'
   get '/500', to: 'errors#internal_server_error'
