3318f95debd4c6b9aae6fbb15c176206c6ccac68
Andrew <56994564+Andriy1328@users.noreply.github.com>
2022-09-02 14:50:38 +0300

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,6 @@ Rails.application.routes.draw do
     root to: "home#index"
     resources :questions, only: %i[index new create show]
     get '/companies', to: 'companies#index'
-  end
 
     get "/404", to: "errors#not_found"
     get '/422', to: 'errors#unprocessable_entity'
