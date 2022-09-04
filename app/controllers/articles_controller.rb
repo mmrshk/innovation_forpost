b03913293a8209e690ca9e66bf1f371b982d8103
Andrew <56994564+Andriy1328@users.noreply.github.com>
2022-09-04 10:48:46 +0300

    Fix rubocop error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  # rubocop:disable Metrics/AbcSize
   def index
     @articles = if !params[:query] || params[:query].empty?
                   Article.includes(:article_tags, :tags).published.in_language(extract_locale).sorted_desc
                 else
-                  Article.includes(:article_tags, :tags).published.in_language(extract_locale).articles_search(params[:query]).sorted_desc
+                  Article.includes(:article_tags,
+                                   :tags).published.in_language(extract_locale).articles_search(params[:query])
                 end
     @tags = Tag.all
   end
+  # rubocop:enable Metrics/AbcSize
 
   def show
     return not_found unless Article.find(params[:id]).published?
