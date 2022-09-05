a332122f6ffb405ff1af5100299151ab0c50213c
Viktoriia Horobets <marakya750@gmail.com>
2022-09-05 12:26:17 +0300

    Fixed

--- spec/models/return_articles_spec.rb
+++ spec/models/return_articles_spec.rb
@@ -5,36 +5,36 @@ require 'rails_helper'
 RSpec.describe ReturnArticle, type: :model do
   let!(:user) { create(:user, email: 'admin@example.com') }
   let!(:article) { create(:article, user: user) }
-  let!(:tag) { create(:tag, name: 'Work')}
+  let!(:tag) { create(:tag, name: 'Work') }
   let!(:article_tag) { create(:article_tag, article: article, tag: tag) }
 
   context 'with a valid data' do
-    it "returns a record" do
-      expect(ReturnArticle.count).to eq 1
+    it 'returns a record' do
+      expect(ReturnArticle.count).to eq(1)
     end
   end
 
   context 'with too old article' do
     let!(:article) { create(:article, user: user, created_at: 5.weeks.ago) }
 
-    it "does not return a record" do
-      expect(ReturnArticle.count).to eq 0
+    it 'does not return a record' do
+      expect(ReturnArticle.count).to eq(0)
     end
   end
 
   context 'with wrong user' do
     let!(:user) { create(:user, email: 'admin1@example.com') }
 
-    it "does not return a record" do
-      expect(ReturnArticle.count).to eq 0
+    it 'does not return a record' do
+      expect(ReturnArticle.count).to eq(0)
     end
   end
 
   context 'with incorrect tag' do
     let!(:tag) { create(:tag, name: 'Job') }
 
-    it "does not return a record" do
-      expect(ReturnArticle.count).to eq 0
+    it 'does not return a record' do
+      expect(ReturnArticle.count).to eq(0)
     end
   end
 end
