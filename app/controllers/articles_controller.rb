b141eefe09bd40f2b15ab530f12c92cfb9e2b652
Andrew <56994564+Andriy1328@users.noreply.github.com>
2022-09-05 18:42:56 +0300

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  # rubocop:disable Metrics/AbcSize
   def index
     @articles = if !params[:query] || params[:query].empty?
                   Article.includes(:article_tags, :tags).published.in_language(extract_locale).sorted_desc
                 else
-                  Article.includes(:article_tags,
-                                   :tags).published.in_language(extract_locale).articles_search(params[:query])
+                  search_result
                 end
     @tags = Tag.all
   end
-  # rubocop:enable Metrics/AbcSize
 
   def show
     return not_found unless Article.find(params[:id]).published?
 
     @article = Article.find(params[:id])
+  end
+
+  def search_result
+    Article.includes(:article_tags,
+                     :tags).published.in_language(extract_locale).articles_search(params[:query]).sorted_desc
   end
 
   def not_found
