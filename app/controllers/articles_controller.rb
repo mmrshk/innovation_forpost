a778c10a4fa269cb91e7ac1b86d86fbea1a582ef
Andrew <56994564+Andriy1328@users.noreply.github.com>
2022-09-06 23:29:59 +0300

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,9 @@
 class ArticlesController < ApplicationController
   def index
     @articles = if !params[:query] || params[:query].empty?
-                  Article.includes(:article_tags, :tags).published.in_language(extract_locale).sorted_desc
+                  articles.sorted_desc
                 else
-                  search_result
+                  articles.articles_search(params[:query]).sorted_desc
                 end
     @tags = Tag.all
   end
@@ -16,12 +16,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def search_result
-    Article.includes(:article_tags,
-                     :tags).published.in_language(extract_locale).articles_search(params[:query]).sorted_desc
-  end
-
   def not_found
     render 'errors/not_found'
+  end
+
+  private
+
+  def articles
+    @articles ||= Article.includes(:article_tags, :tags).published.in_language(extract_locale)
   end
 end
