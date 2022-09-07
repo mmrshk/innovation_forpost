8435cf99bf92fbb954713b92a0009ac2f15c6812
Emma Konstantynovska <emma.konstantynovska@deliveroo.co.uk>
2022-09-07 18:01:40 +0300

    Add env vars

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  config.active_record.encryption.primary_key = "test"
+  config.active_record.encryption.deterministic_key = "test"
+  config.active_record.encryption.key_derivation_salt = "test"
 end
