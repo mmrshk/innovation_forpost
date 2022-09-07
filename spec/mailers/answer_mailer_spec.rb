a0ba8287c5cc2907471eeb99daad317f1f209183
bogdanrymar <bogdanrymar97@gmail.com>
2022-09-07 19:25:11 +0300

    testcommit

--- spec/mailers/answer_mailer_spec.rb
+++ spec/mailers/answer_mailer_spec.rb
@@ -3,16 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe AnswerMailer, type: :mailer do
+  before do
+    allow(Rails.application.credentials.google_account).to receive(:[]).with(:SMTP_USERNAME).and_return(sender_email)
+  end
   describe 'question_answered' do
     subject { described_class.with(answer: answer, question: question, admin: answered_by).question_answered }
     let(:answer) { create(:answer) }
     let(:question) { create(:question) }
     let(:answered_by) { create(:user, :admin) }
-
+    let(:sender_email) { Faker::Internet.email }
     it 'renders the headers' do
       expect(subject.subject).to eq('Question answered')
       expect(subject.to).to eq([question.user_email])
-      expect(subject.from).to eq(['innovation.forpost@gmail.com'])
+      expect(subject.from).to eq([sender_email])
     end
 
     it 'contains necessary info' do
