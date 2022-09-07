1e9783f6ef103089173ea135c551784ab52d9d8d
bogdanrymar <bogdanrymar97@gmail.com>
2022-09-07 18:00:54 +0300

    another fix

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
-
+  config.read_encrypted_secrets = true
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
