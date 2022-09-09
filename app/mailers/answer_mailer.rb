ab9d7bfc22dce9ebf23d6b43b19fc55597f8f810
Emma Konstantynovska <emma.konstantynovska@deliveroo.co.uk>
2022-09-09 17:02:31 +0300

    Fix test

--- app/mailers/answer_mailer.rb
+++ app/mailers/answer_mailer.rb
@@ -5,11 +5,7 @@ class AnswerMailer < ApplicationMailer
     @question = params[:question]
     @answer = params[:answer]
     @admin = params[:admin]
-    @greeting = 'Hi'
 
-    mail(
-      from: Rails.application.credentials[Rails.env.to_sym][:google_account][:SMTP_USERNAME],
-      to: @question.user_email
-    )
+    mail(to: @question.user_email)
   end
 end
