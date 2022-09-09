7bc56b36e161358f27761e0a9acd169cf93bcc1a
Emma Konstantynovska <emma.konstantynovska@deliveroo.co.uk>
2022-09-09 17:24:31 +0300

    Minor refactoring

--- app/controllers/admins/answers_controller.rb
+++ app/controllers/admins/answers_controller.rb
@@ -3,11 +3,13 @@
 module Admins
   class AnswersController < AdminsController
     before_action :question
+
     def create
       @answer = @question.answers.build(answer_params)
 
       if @answer.save
-        AnswerMailer.with(answer: @answer, question: @question, admin: current_user).question_answered.deliver_later
+        AnswerMailer.question_answered(answer: @answer, question: @question, admin: current_user).deliver_later
+
         redirect_to admins_question_path(@question), notice: 'Answer created!'
       else
         @answers = @question.answers.all
@@ -17,6 +19,7 @@ module Admins
 
     def destroy
       answer = @question.answers.find(params[:id])
+
       if answer.destroy
         redirect_to admins_question_path, notice: 'Answer destroyed!'
       else
