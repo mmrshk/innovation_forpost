91af858809e3747c0f8739101f5047bdb136bddf
bogdanrymar <bogdanrymar97@gmail.com>
2022-09-10 23:01:11 +0300

    add mailer language

--- app/mailers/answer_mailer.rb
+++ app/mailers/answer_mailer.rb
@@ -5,7 +5,8 @@ class AnswerMailer < ApplicationMailer
     @question = params[:question]
     @answer = params[:answer]
     @admin = params[:admin]
-
-    mail(to: @question.user_email)
+    I18n.with_locale(@answer.language) do
+      mail(to: @question.user_email)
+    end
   end
 end
