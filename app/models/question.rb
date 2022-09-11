87ad3cba12fa972a063309449a3a9635ba7b1013
bogdanrymar <bogdanrymar97@gmail.com>
2022-09-11 20:03:33 +0300

    add number of unreaded questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
+  scope :without_answer, -> { Question.left_outer_joins(:answers).where(answers: { id: nil }) }
+
   validates :title, presence: true, length: { minimum: 2, maximum: 255 }
   validates :body, presence: true, length: { minimum: 2, maximum: 1000 }
   validates :user_name, presence: true, length: { minimum: 2, maximum: 255 }
