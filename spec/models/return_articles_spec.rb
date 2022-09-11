e3bbd4355419ff7d72a860c9ceea8900ec814853
Viktoriia Horobets <marakya750@gmail.com>
2022-09-11 20:14:54 +0300

    Upd spec

--- spec/models/return_articles_spec.rb
+++ spec/models/return_articles_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe ReturnArticle, type: :model do
   let!(:tag) { create(:tag) }
   let!(:article_tag) { create(:article_tag, article: article, tag: tag) }
   let!(:ck_editor_image) { create(:ck_editor_image) }
+  let(:record) { ReturnArticle.all.to_a.first }
 
   context 'with a valid data' do
     it 'returns a record', :truncation_mode do
@@ -15,15 +16,11 @@ RSpec.describe ReturnArticle, type: :model do
     end
   end
 
-  context 'with tag' do
-    it 'return a record' do
-      expect(ReturnArticle.count).to eq(1)
-    end
-  end
-
   context 'with attached file' do
     it 'return a record' do
-      expect(ReturnArticle.count).to eq(1)
+      expect(record.images_count).to eq(1)
+      expect(record.language).to eq(article.read_attribute_before_type_cast(:language))
     end
   end
+  
 end
