d81d8e9dd0d3fd3e39c75f73a1ec196f9ded9481
bogdanrymar <bogdanrymar97@gmail.com>
2022-09-11 20:21:58 +0300

    try fix preview

--- spec/mailers/previews/answer_mailer_preview.rb
+++ spec/mailers/previews/answer_mailer_preview.rb
@@ -4,6 +4,8 @@
 class AnswerMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/answer_mailer/question_answered
   def question_answered
-    AnswerMailer.with(answer: @answer, question: @question, admin: @admin).question_answered
+    I18n.with_locale(@answer.language) do
+      AnswerMailer.with(Answer.last, Question.last, admin: @admin).question_answered
+    end
   end
 end
