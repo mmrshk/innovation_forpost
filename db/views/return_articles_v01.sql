602582ff48bb3c0e8797fc582ab6203d9799c5eb
Viktoriia Horobets <marakya750@gmail.com>
2022-09-11 11:09:27 +0300

    Fixed uppercase

--- db/views/return_articles_v01.sql
+++ db/views/return_articles_v01.sql
@@ -1,13 +1,13 @@
-Select Articles.id as article_id, 
-	Users.id as user_id, 
+SELECT Articles.id AS article_id, 
+	Users.id AS user_id, 
 	Users.email, 
 	Tags.name, 
 	Articles.status, 
 	Articles.language, 
 	COUNT(ck_editor_images.file ) 
-From Articles
-INNER Join article_tags ON Articles.id = article_tags.id
-INNER Join Users ON Articles.user_id = Users.id
+FROM Articles
+INNER JOIN article_tags ON Articles.id = article_tags.id
+INNER JOIN Users ON Articles.user_id = Users.id
 INNER JOIN tags ON article_tags.tag_id = tags.id 
 INNER JOIN ck_editor_images ON Articles.id = ck_editor_images.id 
 GROUP BY Articles.id, Users.id, Tags.name
