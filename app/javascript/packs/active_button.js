1393dbe3ceb674ae79b240dbb1864a347ee6a61e
Tetiana Bahyra <bagiratana@gmail.com>
2022-09-12 09:54:07 +0300

    Add js file

--- app/javascript/packs/active_button.js
+++ app/javascript/packs/active_button.js
@@ -2,11 +2,6 @@ const items = document.querySelectorAll('.tag-search-item');
 
 items.forEach(item => {
   item.addEventListener("click", function() {   
-   items.forEach(a=>{
-      a.classList.remove("active");
-    });
-     item.classList.add("active");
- 
-    
+     item.classList.toggle("active");
   });  
 });
