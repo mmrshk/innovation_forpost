f27cdc614f7ffa60ad2381fa82ef20283ccbf639
bogdanrymar <bogdanrymar97@gmail.com>
2022-09-12 09:03:09 +0300

    remove coment

--- spec/mailers/previews/answer_mailer_preview.rb
+++ spec/mailers/previews/answer_mailer_preview.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-# Preview all emails at http://localhost:3000/rails/mailers/answer_mailer
 class AnswerMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/answer_mailer/question_answered
   def question_answered
-    I18n.with_locale(@answer.language) do
-      AnswerMailer.with(Answer.last, Question.last, admin: @admin).question_answered
-    end
+    AnswerMailer.with(answer: @answer, question: @question, admin: @admin).question_answered
   end
 end
